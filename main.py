233e79097a6fa8bc2bf4972fd66254b2fe5ff22f
mattlieshout <matthew.m.lieshout@email.shc.edu>
2021-06-06 08:55:55 -0600

    commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, Flask
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisisasecret!'
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    # year = datetime.datetime.now().year
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
